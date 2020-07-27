6ac5e5ae5305bde66edea2e4cadf9a1285346246
Rodolfo Cisneros <brsrkdev@gmail.com>
2020-07-27 01:55:12 -0500

    Add knock, Fix database connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api' do
+    post 'user_token' => 'user_token#create'
+    resources :users
+  end
 end
