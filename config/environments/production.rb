81d920d54f87b526b31a4fbb2e25d8040ab947cf
Rodolfo Cisneros <brsrkdev@gmail.com>
2020-07-27 02:36:14 -0500

    Add secret for prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
+  config.secret_key_base = 'blipblapblup'
 
   config.log_formatter = ::Logger::Formatter.new
 
